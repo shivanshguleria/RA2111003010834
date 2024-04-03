2b6d039fd537f8762949bab5559151aaacebcde4
Shivansh GUleria <shivansh>
2024-04-03 21:20:47 +0530

    a

--- q1/numbers.py
+++ q1/numbers.py
@@ -1,29 +1,30 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, status
+from fastapi import HTTPException
 import requests
 import json, time
 # from ..main import get_auth_token
 router = APIRouter()
-
+times = 0.5
 url = "http://20.244.56.144/test/"
 token_time  = ""
 token = ""
-def get_auth_token():
+def get_auth_token():  
     global token_time, token
-    if token_time < int(time.time()):
+    if token != "" and int(token_time) > int(time.time()):  #Check if token is valid
         return token
     else: 
         const_body = {
-        "companyName": "SRMIST Chennai",
-        "clientID": "7ca3a774-ac1e-4472-b0eb-b819db49a342",
-        "clientSecret": "lsDRcOxShrZuPLby",
-        "ownerName": "Shivansh Guleria",
-        "ownerEmail": "ss2768@srmist.edu.in",
-        "rollNo": "RA2111003010834"
-    }
-        res = requests.post("http://20.244.56.144/test/auth", data=json.dumps(const_body))
+    "companyName": "SRMIST Chennai",
+    "clientID": "39e3a877-d891-4080-8909-210c086f7fa4",
+    "clientSecret": "NnBfzwGxpFtPcTHv",
+    "ownerName": "Shivansh Guleria",
+    "ownerEmail": "ss2768@srmist.edu.in",
+    "rollNo": "RA2111003010834"
+}
+        res = requests.post("http://20.244.56.144/test/auth", data=json.dumps(const_body), timeout=times)
         print(res.json())
         token = res.json()["access_token"]
-        token_time =   res.json()["access_token"] 
+        token_time =   res.json()["expires_in"] 
         return token
 saved_numbers = []
 prev = ""
@@ -33,33 +34,38 @@ def processNumber(req: Request, numberid: str):
     global saved_numbers
     prev = numberid
     if prev != numberid:
+        print("sdfsfd")
         saved_numbers = []
     auth_token = get_auth_token()
     auth_token_s = "Bearer " + auth_token
     print(url + numberid)
-    res = requests.get(url + numberid, headers={"Authorization": auth_token_s})
-    window = 10
-    print(res.text)
-    nums_list = res.json()['numbers']
-    print(nums_list)
-    avg_sum = sum(nums_list)
-
-    if saved_numbers:
-        if saved_numbers[len(saved_numbers)-1 ] == nums_list[0]:
+    try:
+        res = requests.get(url + numberid, headers={"Authorization": auth_token_s}, timeout=times)
+        window = 10
+        print(res.text)
+        nums_list = res.json()['numbers']
+        print(nums_list)
+        avg_sum = sum(nums_list)
+        print(saved_numbers , "adfasf")
+        
+        if saved_numbers and len(saved_numbers) != 0 and saved_numbers[len(saved_numbers)-1 ] == nums_list[0]:  #check if stored list is in continuation
             new_list = saved_numbers.extend(nums_list)
-            avg_sum = sum(new_list[:window])
+            avg_sum = sum(new_list[:window])                     #slice array according to window size
             return {
                         "number": nums_list,
                         "windowPrevState": saved_numbers,
                         "wiondowCurrState": nums_list,
                         avg: avg
                     }
-    else:
-        avg = avg_sum / len(nums_list)
-        saved_numbers.extend(nums_list)
-        return {
-            "number": nums_list,
-            "windowPrevState": saved_numbers,
-            "wiondowCurrState": nums_list,
-            "avg": avg
-        }
+        else:
+
+            avg = avg_sum / len(nums_list)
+            saved_numbers.extend(nums_list)
+            return {
+                "number": nums_list,
+                "windowPrevState": saved_numbers,
+                "wiondowCurrState": nums_list,
+                "avg": avg
+            }
+    except:   #timeout after 500ms
+        raise HTTPException(status_code=408, detail="Query Timeout")

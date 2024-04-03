cd0ed99d27e818c7b401ec62e2aae6cf770b2d10
Shivansh GUleria <shivansh>
2024-04-03 20:20:53 +0530

    a

--- q1/numbers.py
+++ q1/numbers.py
@@ -1,32 +1,39 @@
 from fastapi import APIRouter, Request
 import requests
-import json
+import json, time
 # from ..main import get_auth_token
 router = APIRouter()
 
 url = "http://20.244.56.144/test/"
-
+token_time  = ""
+token = ""
 def get_auth_token():
-    const_body = {
-    "companyName": "SRMIST Chennai",
-    "clientID": "7ca3a774-ac1e-4472-b0eb-b819db49a342",
-    "clientSecret": "lsDRcOxShrZuPLby",
-    "ownerName": "Shivansh Guleria",
-    "ownerEmail": "ss2768@srmist.edu.in",
-    "rollNo": "RA2111003010834"
-}
-    # res = requests.post("http://20.244.56.144/test/auth", data=json.dumps(const_body))
-    # print(res.json())
-    
-    # return res.json()["access_token"]
+    global token_time, token
+    if token_time < int(time.time()):
+        return token
+    else: 
+        const_body = {
+        "companyName": "SRMIST Chennai",
+        "clientID": "7ca3a774-ac1e-4472-b0eb-b819db49a342",
+        "clientSecret": "lsDRcOxShrZuPLby",
+        "ownerName": "Shivansh Guleria",
+        "ownerEmail": "ss2768@srmist.edu.in",
+        "rollNo": "RA2111003010834"
+    }
+        res = requests.post("http://20.244.56.144/test/auth", data=json.dumps(const_body))
+        print(res.json())
+        token = res.json()["access_token"]
+        token_time =   res.json()["access_token"] 
+        return token
 saved_numbers = []
 prev = ""
 @router.get("/numbers/{numberid}")
 def processNumber(req: Request, numberid: str):
     # print(get_auth_token())
+    global saved_numbers
     prev = numberid
     if prev != numberid:
-        nums_list = []
+        saved_numbers = []
     auth_token = get_auth_token()
     auth_token_s = "Bearer " + auth_token
     print(url + numberid)
@@ -47,11 +54,12 @@ def processNumber(req: Request, numberid: str):
                         "wiondowCurrState": nums_list,
                         avg: avg
                     }
-    avg = avg_sum / len(nums_list)
-    saved_numbers.extend(nums_list)
-    return {
-        "number": nums_list,
-        "windowPrevState": saved_numbers,
-        "wiondowCurrState": nums_list,
-        "avg": avg
-    }
+    else:
+        avg = avg_sum / len(nums_list)
+        saved_numbers.extend(nums_list)
+        return {
+            "number": nums_list,
+            "windowPrevState": saved_numbers,
+            "wiondowCurrState": nums_list,
+            "avg": avg
+        }
